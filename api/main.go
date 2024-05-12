e3b11dc9f5f1ab214e52883fd1d3d67ea77cf84b
LoreviQ <lorevi@protonmail.com>
2024-05-12 22:33:49 +0100

    Added default value if .env is missing

--- api/main.go
+++ api/main.go
@@ -14,12 +14,14 @@ type apiConfig struct {
 
 func main() {
 	err := godotenv.Load()
+	port := os.Getenv("PORT")
 	if err != nil {
+		port = "8080"
 		log.Printf("Error loading .env file: %s\n", err)
 	}
 	// Setup Config
 	cfg := apiConfig{
-		port: os.Getenv("PORT"),
+		port: port,
 	}
 	// Initialise Server
 	server := initialiseServer(cfg, http.NewServeMux())
@@ -33,9 +35,8 @@ func initialiseServer(cfg apiConfig, mux *http.ServeMux) *http.Server {
 	mux.HandleFunc("GET /v1/readiness", cfg.getReadiness)
 
 	server := &http.Server{
-		Addr:              ":" + cfg.port,
-		Handler:           mux,
-		ReadHeaderTimeout: 10,
+		Addr:    ":" + cfg.port,
+		Handler: mux,
 	}
 	return server
 }
