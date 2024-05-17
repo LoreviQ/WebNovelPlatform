6719ae18f37e5f8cb9dc723beae01e2fabdf694b
LoreviQ <lorevi@protonmail.com>
2024-05-17 21:48:49 +0100

    Modified schema to account for unique email addresses

--- api/sql/schema/001_users.sql
+++ api/sql/schema/001_users.sql
@@ -3,7 +3,9 @@ CREATE TABLE users (
     id TEXT PRIMARY KEY,
     created_at TEXT NOT NULL,
     updated_at TEXT NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    passwordhash TEXT NOT NULL
 );
 
 -- +goose Down
