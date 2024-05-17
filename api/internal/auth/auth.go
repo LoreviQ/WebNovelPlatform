9ac57dd7b4947b21ea9a0581beb5cf4bbf292d3d
LoreviQ <lorevi@protonmail.com>
2024-05-17 21:56:32 +0100

    Filled in authentication function that checks password hash against db

--- api/internal/auth/auth.go
+++ api/internal/auth/auth.go
@@ -1,18 +1,28 @@
 package auth
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	"github.com/LoreviQ/WebNovelPlatform/api/internal/database"
 	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func authenticateUser(email, password string) {
+func authenticateUser(email string, passwordHash []byte, db *database.Queries) error {
 	// Authenticate user
 	// Check if the user exists
 	// Check if the password is correct
 	// Returns an error if the user does not exist or the password is incorrect
-
+	user, err := db.GetUserByEmail(context.Background(), email)
+	if err != nil {
+		return fmt.Errorf("could not find user: %w", err)
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Passwordhash), passwordHash); err != nil {
+		return fmt.Errorf("incorrect password: %w", err)
+	}
+	return nil
 }
 
 func IssueAccessToken(userID int, secret []byte) (string, error) {
