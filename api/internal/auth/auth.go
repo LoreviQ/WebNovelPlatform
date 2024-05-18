56385e25942d7332cce14c21a7eca7fb639924b4
LoreviQ <lorevi@protonmail.com>
2024-05-18 20:50:30 +0100

    Created function to authenticate issued access tokens

--- api/internal/auth/auth.go
+++ api/internal/auth/auth.go
@@ -36,3 +36,26 @@ func IssueAccessToken(username string, secret []byte) (string, error) {
 	signedToken, err := token.SignedString(secret)
 	return signedToken, err
 }
+
+func AuthenticateAccessToken(tokenString string, secret []byte) (string, error) {
+	// Checks the access token against the provided secret
+	// Returns the username if the token is valid
+	// Returns an error if the token is invalid
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("error parsing token: %w", err)
+	}
+	if !token.Valid {
+		return "", fmt.Errorf("invalid token")
+	}
+	claims, ok := token.Claims.(jwt.RegisteredClaims)
+	if !ok {
+		return "", fmt.Errorf("invalid token claims")
+	}
+	if claims.Issuer != "wnp-access" {
+		return "", fmt.Errorf("invalid token issuer")
+	}
+	return claims.Subject, nil
+}
