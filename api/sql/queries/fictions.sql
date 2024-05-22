41c551d63d63fc1ab81ce651a6a00a9a4f9bcf0c
LoreviQ <lorevi@protonmail.com>
2024-05-22 21:53:26 +0100

    Generated a number of sqlc functions that will be needed to interact with fictions

---
+++ api/sql/queries/fictions.sql
@@ -0,0 +1,24 @@
+-- name: CreateFiction :one
+INSERT INTO fictions (id, title, authorid, description, created_at, updated_at, published_at, published)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+RETURNING *;
+
+-- name: GetFictionById :one
+SELECT * FROM fictions WHERE id = ?;
+
+-- name: GetFictionsByAuthorId :many
+SELECT * FROM fictions WHERE authorid = ?;
+
+-- name: UpdateFiction :one
+UPDATE fictions SET updated_at = ?, title = ?, description = ?, published_at = ?, published = ? 
+WHERE id = ? 
+RETURNING *;
+
+-- name: PublishFiction :one
+UPDATE fictions SET published_at = ?, published = ? WHERE id = ? RETURNING *;
+
+-- name: DeleteFiction :one
+DELETE FROM fictions WHERE id = ? RETURNING *;
+
+-- name: GetPublishedFictions :many
+SELECT * FROM fictions WHERE published = 1;
