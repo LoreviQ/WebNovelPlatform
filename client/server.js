155bd9404636711406ac8b2fdf32675eb5e4d2ea
LoreviQ <lorevi@protonmail.com>
2024-06-13 01:55:40 +0100

    Updated server.js to serve html files if not specified

--- client/server.js
+++ client/server.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 8000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Serve HTML files without the .html extension
+app.get("/:page", (req, res) => {
+    const page = req.params.page;
+    res.sendFile(path.join(__dirname, "public", `${page}.html`));
+});
+
+// Catch-all route to serve index.html for any other route
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
