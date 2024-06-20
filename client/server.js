5ab45f7482006fd8b5a8b87089c0195a9cb1a40a
LoreviQ <lorevi@protonmail.com>
2024-06-20 23:23:56 +0100

    Added dynamic page based on logged in user

--- client/server.js
+++ client/server.js
@@ -4,8 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.set("view engine", "ejs");
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
+
+// Serve User Pages Based on url
+app.get("/user/:userId/fictions", (req, res) => {
+    const userId = req.params.userId;
+    res.render("user_fictions", { userId: userId });
+});
 
 // Serve HTML files without the .html extension
 app.get("/:page", (req, res) => {
