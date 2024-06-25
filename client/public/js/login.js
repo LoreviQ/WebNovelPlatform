f02987d4d78114dd3fe7aa839e093629ba1ed195
LoreviQ <lorevi@protonmail.com>
2024-06-25 17:36:12 +0100

    Removed depreciated code

--- client/public/js/login.js
+++ client/public/js/login.js
@@ -2,8 +2,6 @@
 Code for the login page at baseurl/login
 */
 
-const newAuth = true;
-
 window.addEventListener("DOMContentLoaded", function () {
     const apiBaseUrl = "https://webnovelapi-y5hewbdc4a-nw.a.run.app"; // Change this to your actual API base URL
     // Handle form submission for logging in
@@ -21,63 +19,30 @@ window.addEventListener("DOMContentLoaded", function () {
             password: password,
         };
 
-        if (newAuth) {
-            const currentDomain = `${window.location.protocol}//${window.location.host}`;
-            fetch(currentDomain + "/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(postData),
+        const currentDomain = `${window.location.protocol}//${window.location.host}`;
+        fetch(currentDomain + "/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(postData),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    // Check if response status is 200
+                    window.location.href = "/";
+                } else {
+                    throw new Error(
+                        "Status:" +
+                            response.status +
+                            "\nFailed to login: " +
+                            response.statusText
+                    );
+                }
             })
-                .then((response) => {
-                    if (response.ok) {
-                        // Check if response status is 200
-                        window.location.href = "/";
-                    } else {
-                        throw new Error(
-                            "Status:" +
-                                response.status +
-                                "\nFailed to login: " +
-                                response.statusText
-                        );
-                    }
-                })
-                .catch((error) => {
-                    console.error("Error logging in:", error);
-                    alert("Invalid Username or Password.");
-                });
-        } else {
-            // Make AJAX POST request
-            fetch(apiBaseUrl + "/v1/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(postData),
-            })
-                .then((response) => {
-                    // Check if the response status is 200 (OK)
-                    if (response.status === 200) {
-                        return response.json();
-                    } else {
-                        throw new Error(
-                            "Status:" +
-                                response.status +
-                                "\nFailed to login: " +
-                                response.statusText
-                        );
-                    }
-                })
-                .then((data) => {
-                    localStorage.setItem("accessToken", data.token);
-                    localStorage.setItem("refreshToken", data.refresh);
-                    window.location.href = "/"; // Redirect to the home page
-                })
-                .catch((error) => {
-                    console.error("Error logging in:", error);
-                    alert("Invalid Username or Password.");
-                });
-        }
+            .catch((error) => {
+                console.error("Error logging in:", error);
+                alert("Invalid Username or Password.");
+            });
     });
 });
