0b400b5618773db65ed96db3ceb8858bbf98114c
LoreviQ <lorevi@protonmail.com>
2024-06-26 16:57:36 +0100

    Added routing to react app

--- react/client/src/index.js
+++ react/client/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Index from "./pages/index";
+
+function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App Page={Index} />}>
+                    <Route path="index" element={<App Page={Index} />} />
+                    <Route path="home" element={<App Page={Index} />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App Page={Index} />
+        <AppRouter />
     </React.StrictMode>
 );
