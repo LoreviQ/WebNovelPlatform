339cb37c51fdf51faf2aadc09acaecf6ee767dae
LoreviQ <lorevi@protonmail.com>
2024-06-26 17:49:56 +0100

    Added title to index page

--- react/client/src/pages/index.js
+++ react/client/src/pages/index.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Index() {
+    useEffect(() => {
+        document.title = "Home | WebNovelPlatform";
+    }, []);
     return <h1>Index Page here</h1>;
 }
 
