56001c69a691b2a53198eb4a7dbbb8d42972807e
LoreviQ <lorevi@protonmail.com>
2024-07-01 18:28:10 +0100

    Created form for submission

--- client/src/pages/submit-fiction.js
+++ client/src/pages/submit-fiction.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 import { useAuth } from "../utils/auth";
 import { GetUserByUID } from "../utils/api";
@@ -17,9 +21,35 @@ function SubmitFiction() {
         <div className="container-fluid px-4">
             <div style={{ display: "flex", alignItems: "center" }}>
                 <h1 className="mt-4">Submit your fiction!</h1>
+                <div style={{ flexGrow: 1 }}></div>
+                <Button className="mt-4 me-4" variant="theme">
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <FontAwesomeIcon className="ms-1 mt-1 me-2" icon={faPlus} size="2x" />
+                        <h2 className="mt-2 me-1">Submit</h2>
+                    </div>
+                </Button>
             </div>
             <hr />
-            <h1>Form here</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="submitTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="title" placeholder="Title of Fiction" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="submitDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control type="description" placeholder="Summary of your fiction" />
+                </Form.Group>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <div style={{ flexGrow: 1 }}></div>
+                    <Button className="mt-4 me-4" variant="theme">
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <FontAwesomeIcon className="ms-1 mt-1 me-2" icon={faPlus} size="2x" />
+                            <h2 className="mt-2 me-1">Submit</h2>
+                        </div>
+                    </Button>
+                </div>
+            </Form>
         </div>
     );
 }
