f4b1c759f0780a0d089ef5e8673397e9de7c52c5
LoreviQ <lorevi@protonmail.com>
2024-07-25 19:55:18 +0100

    Refactored middleware to reduice duplicate code

--- api/middleware.go
+++ api/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/LoreviQ/WebNovelPlatform/api/internal/auth"
@@ -9,33 +10,19 @@ import (
 
 type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
+// Middleware to authenticate requests
 func (cfg *apiConfig) AuthMiddleware(next authedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// GET ACCESS TOKEN FROM HEADER
-		header := r.Header.Get("Authorization")
-		if header == "" {
-			respondWithError(w, http.StatusUnauthorized, "No Authorization Header")
-			return
-		}
-		token := header[len("Bearer "):]
-
-		// AUTHENTICATE ACCESS TOKEN
-		ID, err := auth.AuthenticateAccessToken(token, cfg.JWT_Secret)
+		user, err := cfg.authenticateRequest(r)
 		if err != nil {
 			respondWithError(w, http.StatusUnauthorized, err.Error())
-			return
-		}
-
-		// GET USER BY ID
-		user, err := cfg.DB.GetUserById(r.Context(), ID)
-		if err != nil {
-			respondWithError(w, http.StatusUnauthorized, "Invalid subject in token")
 			return
 		}
 		next(w, r, user)
 	}
 }
 
+// Middleware that only authenticates requests if the fiction is not published
 func (cfg *apiConfig) IsPublishedMiddleware(next authedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
@@ -47,23 +34,9 @@ func (cfg *apiConfig) IsPublishedMiddleware(next authedHandler) http.HandlerFunc
 		if fiction.Published == 1 {
 			next(w, r, database.User{})
 		} else {
-			// GET ACCESS TOKEN FROM HEADER
-			header := r.Header.Get("Authorization")
-			if header == "" {
-				respondWithError(w, http.StatusUnauthorized, "No Authorization Header")
-				return
-			}
-			token := header[len("Bearer "):]
-			// AUTHENTICATE ACCESS TOKEN
-			ID, err := auth.AuthenticateAccessToken(token, cfg.JWT_Secret)
+			user, err := cfg.authenticateRequest(r)
 			if err != nil {
 				respondWithError(w, http.StatusUnauthorized, err.Error())
-				return
-			}
-			// GET USER BY ID
-			user, err := cfg.DB.GetUserById(r.Context(), ID)
-			if err != nil {
-				respondWithError(w, http.StatusUnauthorized, "Invalid subject in token")
 				return
 			}
 			next(w, r, user)
@@ -82,4 +55,28 @@ func (cfg *apiConfig) CorsMiddleware(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+// Helper function to auth requests
+func (cfg *apiConfig) authenticateRequest(r *http.Request) (database.User, error) {
+	// GET ACCESS TOKEN FROM HEADER
+	header := r.Header.Get("Authorization")
+	if header == "" {
+		return database.User{}, fmt.Errorf("no Authorization Header")
+	}
+	token := header[len("Bearer "):]
+
+	// AUTHENTICATE ACCESS TOKEN
+	ID, err := auth.AuthenticateAccessToken(token, cfg.JWT_Secret)
+	if err != nil {
+		return database.User{}, err
+	}
+
+	// GET USER BY ID
+	user, err := cfg.DB.GetUserById(r.Context(), ID)
+	if err != nil {
+		return database.User{}, fmt.Errorf("invalid subject in token")
+	}
+
+	return user, nil
 }
