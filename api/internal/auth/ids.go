e1a8c899ae4978d6a9edfc3d15ba738f31101471
LoreviQ <lorevi@protonmail.com>
2024-08-01 22:38:41 +0100

    Created a function to generate urlfriendly ids

---
+++ api/internal/auth/ids.go
@@ -0,0 +1,15 @@
+package auth
+
+import (
+	"math/rand"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateID(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
